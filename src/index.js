584a9d1ca1b0617ad97779cd3937f841908f68d4
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2016-12-23 00:15:51 -0500

    testing build size of ramda with different fns

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-// import compose from "ramda/src/compose"
+import compose from "ramda/src/compose"
+import prop from "ramda/src/prop"
 import { hellaUsed } from "./utils/uses"
 
 console.log(hellaUsed())
+
+const a = x => x + 1
+const b = x => x + 2
+
+const c = compose(a, b)
+console.log(c(4))
+
+let d = {
+    e: 6
+}
+
+let f = prop('e', d)
+console.log(f)
