9afa4859c7f1026a3f1d152778d379731b4a9c4d
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2016-12-23 11:39:14 -0500

    finished app, I hate jquery...

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
+import Task from "data.task"
 import compose from "ramda/src/compose"
 import prop from "ramda/src/prop"
+import map from "ramda/src/map"
+import $ from "jquery"
 import { hellaUsed } from "./utils/uses"
 
-console.log(hellaUsed())
+const url = t =>
+    `http://api.flickr.com/services/feeds/photos_public.gne?tags=${t}&format=json&jsoncallback=?`
 
-const a = x => x + 1
-const b = x => x + 2
+const jsonToItems = prop("items")
 
-const c = compose(a, b)
-console.log(c(4))
-
-let d = {
-    e: 6
+const mediaToUrl = compose(prop("m"), prop("media"))
+const img = url => {
+    const ret = new Image()
+    ret.src = url
+    return ret
 }
 
-let f = prop('e', d)
-console.log(f)
+const mediaToImg = compose(img, mediaToUrl)
+
+const itemsToImages = map(mediaToImg)
+
+//impure
+const renderImages = imgs => {
+    for (let i of imgs) {
+        document.body.appendChild(i)
+    }
+}
+
+const trace = x => {
+    console.log(x)
+    return x;
+}
+
+const getJSON = uri =>
+    new Task((rej, res) =>
+        $.getJSON(uri)
+        .done(data => res(data))
+        .fail((_, __, error) => rej(error)))
+
+const app = uri => 
+    getJSON(uri)
+    .map(trace)
+    .map(jsonToItems)
+    .map(itemsToImages)
+    .map(renderImages)
+
+const cats = url("dogs")
+
+app(cats)
+.fork(console.error, console.log)
